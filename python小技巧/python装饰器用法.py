84726ac284739e94de6d8a68140e47e2dd412988
caojingwoaini <931061950@qq.com>
2018-05-12 17:35:52 +0800

    gfgf

--- python小技巧/python装饰器用法.py
+++ python小技巧/python装饰器用法.py
@@ -5,8 +5,8 @@ from functools import reduce
 
 
 
-def filter2(map2):
-    def filter3(map2_list):
+def filter2(map2):   #在装饰器里，主函数将被装饰函数当作参数
+    def filter3(map2_list):  #在装饰器里，子函数将被装饰函数的参数当作参数
         filter_list=map2(map2_list)
         return list(filter(lambda y:y%2==0,filter_list))
     return filter3
