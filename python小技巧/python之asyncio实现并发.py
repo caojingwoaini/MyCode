7bd490a16fe04fbd4f1194310c78139e47d82848
caojingwoaini <931061950@qq.com>
2018-09-11 18:18:08 +0800

    Update python之asyncio实现并发.py

--- python小技巧/python之asyncio实现并发.py
+++ python小技巧/python之asyncio实现并发.py
@@ -31,3 +31,33 @@ loop.close()
 
 
 
+
+#coding:utf-8
+
+import asyncio
+import time
+import  pymysql
+import urllib.request as urllib2
+import codecs
+
+file=open("E:/火眼风控/性能测试数据/gaodajun.txt","r",encoding="utf-8")
+fileContent=file.read()
+file.close()
+fileContentList=fileContent.split("\n")
+
+@asyncio.coroutine
+def tongdunThread(i):
+    print(fileContentList[i])
+
+
+loop=asyncio.get_event_loop()
+tasks=[tongdunThread(i) for i in range(4)]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+
+#高大俊1,350424195501082038,tt61bbdc859c4ab1a0ec0d2h04e54000,e0a49735aae61c6bdcc99c03a6fa3000
+#高大俊2,350424195501082039,tt61bbdc859c4ab1a0ec0d2h04e54001,e0a49735aae61c6bdcc99c03a6fa3001
+#高大俊3,350424195501082040,tt61bbdc859c4ab1a0ec0d2h04e54002,e0a49735aae61c6bdcc99c03a6fa3002
+#高大俊4,350424195501082041,tt61bbdc859c4ab1a0ec0d2h04e54003,e0a49735aae61c6bdcc99c03a6fa3003
+
+
